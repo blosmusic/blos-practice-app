421d5077fc2ef88b8e3364b3c870edfecf2ca778
Ben Lamb O'Sullivan <64701953+blosmusic@users.noreply.github.com>
2022-09-06 17:13:54 +0100

    basic tuner styling

--- assests/tuner.js
+++ assests/tuner.js
@@ -1,33 +1,62 @@
-const audioContext = new AudioContext();
+let tunerButton = document.getElementById("tuner-indication");
+let tunerIsRunning = false;
 
-navigator.permissions.query({ name: "microphone" }).then(function (result) {
-  if (result.state == "granted") {
-    console.log("Microphone access granted");
-    //TODO: start app
-  } else if (result.state == "prompt") {
-    console.log("Microphone access prompt");
-    
-
-  } else if (result.state == "denied") {
-    console.log("Microphone access denied");
-    //TODO: show error message
-
+tunerButton.onclick = function () {
+  if (!tunerIsRunning) {
+    startTuner();
+  } else {
+    stopTuner();
   }
-  result.onchange = function () {};
-});
+};
 
-const tuner = document.querySelector(".tuner");
-const tunerDisplay = document.querySelector(".tuner-display");
-const tunerStartStop = document.querySelector(".tuner-start-stop");
-const tunerIsRunning = false;
+function startTuner() {
+  console.log("tuner started");
+  tunerButton.innerText = "STOP";
+  tunerIsRunning = true;
+  document.getElementById("tuner-indication").style.backgroundColor = "black";
+}
 
-tunerStartStop.addEventListener("click", function () {
-    if (!tunerIsRunning) {
-        console.log("tuner started");
-        tunerStartStop.innerText = "STOP";
+function stopTuner() {
+  console.log("tuner stopped");
+  tunerIsRunning = false;
+  tunerButton.innerText = "START";
+  document.getElementById("tuner-indication").style.backgroundColor = "red";
+}
 
-    } else {
-        console.log("tuner stopped");
-        tunerStartStop.innerText = "START";
+if (tunerIsRunning === true) {
+    let audioContext = new AudioContext();
+    let analyser = audioContext.createAnalyser();
+    let microphone = audioContext.createMediaStreamSource(stream);
+    let javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+    analyser.smoothingTimeConstant = 0.3;
+    analyser.fftSize = 1024;
+    microphone.connect(analyser);
+    analyser.connect(javascriptNode);
+    javascriptNode.connect(audioContext.destination);
+    javascriptNode.onaudioprocess = function () {
+        let array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        let values = 0;
+        let length = array.length;
+        for (let i = 0; i < length; i++) {
+        values += array[i];
+        }
+        let average = values / length;
+        console.log(Math.round(average));
+    };
     }
-});
+    
+    //
+
+// navigator.permissions.query({ name: "microphone" }).then(function (result) {
+//   if (result.state == "granted") {
+//     console.log("Microphone access granted");
+//     //TODO: start app
+//   } else if (result.state == "prompt") {
+//     console.log("Microphone access prompt");
+//   } else if (result.state == "denied") {
+//     console.log("Microphone access denied");
+//     //TODO: show error message
+//   }
+//   result.onchange = function () {};
+// });
