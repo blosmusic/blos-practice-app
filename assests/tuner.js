c5eb84c46c7e63694bc3d4cb8b8afff346d58158
Ben Lamb O'Sullivan <64701953+blosmusic@users.noreply.github.com>
2022-10-01 17:28:54 +0100

    tuner boilerplate

--- assests/tuner.js
+++ assests/tuner.js
@@ -13,6 +13,7 @@ function startTuner() {
   console.log("tuner started");
   tunerButton.innerText = "STOP";
   tunerIsRunning = true;
+  activeTuner();
   document.getElementById("tuner-indication").style.backgroundColor = "black";
 }
 
@@ -23,30 +24,33 @@ function stopTuner() {
   document.getElementById("tuner-indication").style.backgroundColor = "red";
 }
 
-if (tunerIsRunning === true) {
-    let audioContext = new AudioContext();
-    let analyser = audioContext.createAnalyser();
-    let microphone = audioContext.createMediaStreamSource(stream);
-    let javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
-    analyser.smoothingTimeConstant = 0.3;
-    analyser.fftSize = 1024;
-    microphone.connect(analyser);
-    analyser.connect(javascriptNode);
-    javascriptNode.connect(audioContext.destination);
-    javascriptNode.onaudioprocess = function () {
-        let array = new Uint8Array(analyser.frequencyBinCount);
-        analyser.getByteFrequencyData(array);
-        let values = 0;
-        let length = array.length;
-        for (let i = 0; i < length; i++) {
-        values += array[i];
-        }
-        let average = values / length;
-        console.log(Math.round(average));
-    };
-    }
-    
-    //
+function activeTuner() {
+  console.log("tuner is running");
+  if (tunerIsRunning === true) {
+    console.log("fft data");
+    //todo create functional fft analysis
+    // let audioContext = new AudioContext();
+    // let analyser = audioContext.createAnalyser();
+    // let microphone = audioContext.createMediaStreamSource(stream);
+    // let javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+    // analyser.smoothingTimeConstant = 0.3;
+    // analyser.fftSize = 1024;
+    // microphone.connect(analyser);
+    // analyser.connect(javascriptNode);
+    // javascriptNode.connect(audioContext.destination);
+    // javascriptNode.onaudioprocess = function () {
+    //     let array = new Uint8Array(analyser.frequencyBinCount);
+    //     analyser.getByteFrequencyData(array);
+    //     let values = 0;
+    //     let length = array.length;
+    //     for (let i = 0; i < length; i++) {
+    //     values += array[i];
+    //     }
+    //     let average = values / length;
+    //     console.log(Math.round(average));
+    // };
+  }
+}
 
 // navigator.permissions.query({ name: "microphone" }).then(function (result) {
 //   if (result.state == "granted") {
