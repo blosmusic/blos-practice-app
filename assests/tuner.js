c15b9e3ecadf144205205879850f1144068fcafc
Ben Lamb O'Sullivan <64701953+blosmusic@users.noreply.github.com>
2022-11-03 19:28:39 +0000

    prep tunner section and style

--- assests/tuner.js
+++ assests/tuner.js
@@ -17,14 +17,13 @@ function startTuner() {
   console.log("tuner started");
   tunerButton.innerText = "STOP";
   tunerIsRunning = true;
-  document.getElementById("tuner-indication").style.backgroundColor = "black";
+  document.getElementById("tuner-indication").style.backgroundColor = "#00ff9f";
   
   mic
   .open()
   .then(() => {
     console.log("mic opened");
-    startPitch();
-    processAudioInputLevel();
+    // what to do with the mic
   })
     .catch((e) => {
       console.log("mic error", e);
@@ -34,51 +33,19 @@ function startTuner() {
 
 function stopTuner() {
   console.log("tuner stopped");
-  tunerIsRunning = false;
   tunerButton.innerText = "START";
+  tunerIsRunning = false;
   document.getElementById("tuner-indication").style.backgroundColor = "red";
   mic.close();
 }
 
-function processAudioInputLevel() {
-  console.log("processAudioInputLevel called");
-  inputLevelValueRead = meter.getValue().toFixed(2);
-  // print the incoming mic levels in decibels
-  console.log("The Decibel level is:", inputLevelValueRead, "dB");
-}
+// todo compare pitch to closest note in chromatic scale and display note name 
 
-function startPitch() {
-  pitch = ml5.pitchDetection("./model/", audioContext, mic.stream, modelLoaded);
-}
 
-function modelLoaded() {
-  // select("#tuner-indication").html("Model Loaded");
-  console.log("model loaded");
-  getPitch();
-}
+// todo check if note is in key and display note name in green if it is, red if it isn't
 
-function getPitch() {
-  pitch.getPitch(function (err, frequency) {
-    if (frequency) {
-      console.log(frequency, "Hz");
-      // select("#tuner-indication").html(frequency);
-    } else {
-      console.log("no pitch");
-      // select("#tuner-indication").html("No pitch detected");
-    }
-    getPitch();
-  });
-}
 
-// navigator.permissions.query({ name: "microphone" }).then(function (result) {
-//   if (result.state == "granted") {
-//     console.log("Microphone access granted");
-//     //TODO: start app
-//   } else if (result.state == "prompt") {
-//     console.log("Microphone access prompt");
-//   } else if (result.state == "denied") {
-//     console.log("Microphone access denied");
-//     //TODO: show error message
-//   }
-//   result.onchange = function () {};
-// });
+// todo highlight flat or sharp indicator if note is flat or sharp
+
+
+// measure the difference in pitch as cents and display
